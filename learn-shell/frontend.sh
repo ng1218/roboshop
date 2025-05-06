f6d66706690670e7b74e66dd38a3a834b7e35f55
i340771 <naresh.ganta@sap.com>
2025-05-06 21:45:04 +0530

    updated frontend

--- learn-shell/frontend.sh
+++ learn-shell/frontend.sh
@@ -3,6 +3,7 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+cp nginx.conf /etc./nginx/nginx.conf
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 systemctl restart nginx 
