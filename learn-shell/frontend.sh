3edb9971c00ea202116b3d3a4b03a39645c7187e
i340771 <naresh.ganta@sap.com>
2025-05-07 08:17:33 +0530

    test

--- learn-shell/frontend.sh
+++ learn-shell/frontend.sh
@@ -1,9 +1,9 @@
 dnf install nginx -y 
 systemctl enable nginx 
 systemctl start nginx
+cp nginx.conf /etc/nginx/nginx.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-#cp nginx.conf /etc/nginx/nginx.conf
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 systemctl restart nginx 
