4407993049effa7abf707f505138c3fb79c5e79c
i340771 <naresh.ganta@sap.com>
2025-05-23 16:26:41 +0530

    test

--- learn-shell/roboshop/catalogue.sh
+++ learn-shell/roboshop/catalogue.sh
@@ -3,6 +3,7 @@ component_name=catalogue
 
 nodejs
 
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org-shell -y
 mongo --host mongodb-dev.nareshdevops1218.online </app/db/master-data.js
 
