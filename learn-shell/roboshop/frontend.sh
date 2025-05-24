745cf16b3c8b93e1d08efd302504848dd7578352
i340771 <naresh.ganta@sap.com>
2025-05-24 12:12:12 +0530

    test

--- learn-shell/roboshop/frontend.sh
+++ learn-shell/roboshop/frontend.sh
@@ -8,7 +8,7 @@ systemctl start nginx
 cp nginx.conf /etc/nginx/nginx.conf
 echo 'deleting default nginx html'
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 systemctl restart nginx 
